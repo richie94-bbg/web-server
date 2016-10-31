0b73b186084d091d9234af255c932351f7f2cba7
richie94-bbg <rdrobin94@bu.edu>
2016-10-30 22:08:39 -0400

    Initialized repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var PORT = 3000; //uppercase variable name is saying it shouldn't change
+
+// app.get('/',function(req, res){ //get responds to http request methof
+// 	res.send("Hello Express!");
+// });
+
+var middleware = {
+	requireAuthentication: function(req,res,next){
+		console.log("private route hit!");
+		next();
+	},
+	logger: function(req,res,next){
+		console.log("Request: " + new Date().toString() + " " + req.method + " " + req.originalUrl);
+		next()
+	}
+};
+
+app.use(middleware.logger); //important to put middleware uo top to run
+
+app.get("/about",middleware.requireAuthentication,middleware.logger,function(req,res){
+	res.send("About Us");
+});
+
+app.use(express.static(__dirname + "/public")); //takes folder you wnt to expose
+
+//console.log(__dirname); //gets path of directory
+app.listen(PORT,function(){
+	console.log("Express server started with port " + PORT + " !" );
+}); //what port to listen
